8bcf4fcd4f0f4e96f51fbec55400cba73fea0812
Administrator <123123123@gmail.com>
2026-07-28 22:04:56 +0800

    优化数据库链接信息
    (cherry picked from commit 4f29838961918514c90d80f0b0f291436effab58)

--- ruoyi-modules/ruoyi-chat/src/main/java/org/ruoyi/agent/EchartsAgent.java
+++ ruoyi-modules/ruoyi-chat/src/main/java/org/ruoyi/agent/EchartsAgent.java
@@ -38,23 +38,54 @@ public interface EchartsAgent {
         {
           "title": {
             "text": "Dict Type Distribution",
-            "left": "center"
+            "left": "center",
+            "textStyle": {
+              "fontFamily": "SimSun, serif"
+            }
           },
           "tooltip": {
             "trigger": "axis"
           },
+          "grid": {
+            "left": "3%",
+            "right": "4%",
+            "bottom": "15%",
+            "top": "15%",
+            "containLabel": true
+          },
           "xAxis": {
             "type": "category",
-            "data": ["type1", "type2", "type3"]
+            "data": ["type1", "type2", "type3"],
+            "axisLabel": {
+              "fontFamily": "SimSun, serif",
+              "fontSize": 12,
+              "width": 80,
+              "overflow": "truncate",
+              "ellipsis": "...",
+              "rotate": 30,
+              "hideOverlap": true,
+              "lineHeight": 20
+            },
+            "axisTick": {
+              "alignWithLabel": true
+            }
           },
           "yAxis": {
-            "type": "value"
+            "type": "value",
+            "axisLabel": {
+              "fontFamily": "SimSun, serif",
+              "fontSize": 12
+            }
           },
           "series": [
             {
               "name": "数量",
               "type": "bar",
-              "data": [10, 20, 15]
+              "data": [10, 20, 15],
+              "label": {
+                "fontFamily": "SimSun, serif",
+                "fontSize": 12
+              }
             }
           ]
         }
@@ -62,7 +93,7 @@ public interface EchartsAgent {
 
         For pie charts:
         {
-          "title": {"text": "Chart Title", "left": "center"},
+          "title": {"text": "Chart Title", "left": "center", "textStyle": {"fontFamily": "SimSun, serif"}},
           "tooltip": {"trigger": "item"},
           "series": [{
             "type": "pie",
@@ -70,14 +101,34 @@ public interface EchartsAgent {
             "data": [
               {"value": 10, "name": "Category1"},
               {"value": 20, "name": "Category2"}
-            ]
+            ],
+            "label": {
+              "fontFamily": "SimSun, serif",
+              "fontSize": 12
+            }
           }]
         }
+
+        FONT AND LABEL DISPLAY RULES (CRITICAL for Chinese fonts):
+        - ALWAYS set fontFamily to "SimSun, serif" for ALL text elements (title, xAxis.axisLabel, yAxis.axisLabel, series.label, legend, etc.)
+        - Set fontSize to 12 for axis labels and series labels, 14 for titles
+        - For xAxis with category data:
+          - Set grid.bottom to "15%" (minimum) to provide enough space for rotated labels
+          - Set axisLabel.rotate to 30 degrees for long category names
+          - Set axisLabel.width to 80 to limit label width
+          - Set axisLabel.overflow to "truncate" and axisLabel.ellipsis to "..." for long text
+          - Set axisLabel.hideOverlap to true to hide overlapping labels
+          - Set axisLabel.lineHeight to 20 for proper spacing
+          - Set axisTick.alignWithLabel to true
+        - Set grid.containLabel to true to ensure labels are contained within the grid
+        - For yAxis: always set axisLabel.fontFamily to "SimSun, serif"
+        - For series labels: always set label.fontFamily to "SimSun, serif"
 
         REMEMBER:
         - Always wrap JSON in ```json and ``` markers
         - Use proper formatting with indentation
         - This is the expected format for frontend parsing
+        - Proper font configuration is CRITICAL for Chinese character rendering and export to Word documents
         """)
     @UserMessage("""
         Generate an Echarts chart for: {{query}}
